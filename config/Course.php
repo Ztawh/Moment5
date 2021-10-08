e41ea09109a6d0ddd3e52f28ac77388df2721a82
Amanda Hwatz Björkholm <amandahwatz@gmail.com>
2021-10-08 14:55:20 +0200

    Update course

--- config/Course.php
+++ config/Course.php
@@ -12,7 +12,7 @@ class Course
                 die('Fel vid anslutning [' . $this->db->connect_error . ']');
             }
         } else {
-            $this->db = new mysqli('studentmysql.miun.se', 'amhv2000', 'CA2gMHbpLX', 'amhv2000');
+            $this->db = new mysqli(host, 'amhv2000', lösen, 'amhv2000');
             if ($this->db->connect_errno > 0) {
                 die('Fel vid anslutning [' . $this->db->connect_error . ']');
             }
